fd4ded6d3a572b0eefbb95277fbde5024eafaf16
Des <d.stout31@yahoo.com>
2021-02-12 22:28:11 -0600

    added local host mysql connection to js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 //Require modules
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+
+// Set up mysql local host connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    
+    port: 3001,
+
+    user: 'root',
+
+    password: '',
+    database:  '',
+});
