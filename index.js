536bd6357856f1e656c3b83f7626326a09fddb0d
Des <d.stout31@yahoo.com>
2021-02-12 22:53:08 -0600

    added first list of prompts in inquirer array

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Require modules
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const Choice = require("inquirer/lib/objects/choice");
 
 // Set up mysql local host connection
 const connection = mysql.createConnection({
@@ -13,3 +14,26 @@ const connection = mysql.createConnection({
   password: "",
   database: "employeeDB",
 });
+
+//Inquirer questions
+const employeeInfo = () => {
+    inquirer
+        .prompt({
+            type: 'list',
+            name: 'selection',
+            message: 'What would you like to do?'
+            choices: [
+                "View all employees",
+                "View all departments",
+                "View all roles",
+                "Add employee",
+                "Add department",
+                "Add role",
+                "Update employee role",
+                "Delete employee",
+            ],
+        })
+
+
+        
+};
